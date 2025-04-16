3c84926e63340ddecbe0aa7bc248f9b0d3887ce7
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 08:15:58 +0700

    menyelesaikan program dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace modul8_103022300158
+{
+    class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public string transfer_threshold { get; set; }
+        public string transfer_low_fee { get; set; }
+        public string transfer_high_fee { get; set; }
+        public string[] methods { get; set; }
+        public string confirmation { get; set; }
+
+        private static readonly string configFilePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\bank_transfer_config.json"));
+
+        public BankTransferConfig()
+        {
+            ReadConfigFile();
+        }
+
+        public void UbahBahasa(string newLang)
+        {
+            if (lang == "en")
+            {
+                lang = "id";
+            }
+            else
+            {
+                lang = "en";
+            }
+            WriteNewConfig();
+        }
+
+        public static BankTransferConfig ReadConfigFile()
+        {
+            BankTransferConfig config;
+            if (File.Exists(configFilePath)) {
+                string json = File.ReadAllText(configFilePath);
+                config = JsonSerializer.Deserialize<BankTransferConfig>(json);
+            }
+            else
+            {
+                config = new BankTransferConfig();
+                config.WriteNewConfig();
+            }
+            return config;
+        }
+        private void setDefaultConfig()
+        {
+            lang = "eng";
+            transfer_threshold = "25000000";
+            transfer_low_fee = "6500";
+            transfer_high_fee = "15000";
+            methods = ["“RTO", "(real-time)”", "“SKN”", "“RTGS”", "“BI", "FAST”"];
+            confirmation = "yes";
+            WriteNewConfig();
+        }
+
+        private void WriteNewConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(this, options);
+            File.WriteAllText(configFilePath, json);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+using modul8_103022300158;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config = BankTransferConfig.ReadConfigFile();
+        bool checkLanguage = config.lang.ToLower() switch
+        {
+            "en" => true,
+            "id" => false,
+            _ => true
+        };
+
+        Console.WriteLine("Current Language: " + (checkLanguage ? "English" : "Indonesian"));
+    }
+}
